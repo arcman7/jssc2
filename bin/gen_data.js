3ddd243c03294f0593662c5af32676195e5ee87f
Jihan Kim <jihan1218@gmail.com>
2020-09-17 21:09:02 -0700

    init gen_data.js

---
+++ bin/gen_data.js
@@ -0,0 +1,5 @@
+// Generate the unit definitions for units.py.
+const path = require('path')
+const s2clientprotocol = require('s2clientprotocol')
+
+const maps = require(path.resolve(__dirname, '..', 'maps'))
